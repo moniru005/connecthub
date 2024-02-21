ff956bd523c86a6eb4792fb9d7f47a02d2e5c7be
ashimroyrupok <ashimroyrupok@gmail.com>
2024-02-22 01:49:33 +0600

    birthday card is ready

--- src/components/RightSideBar/RightRideBar.jsx
+++ src/components/RightSideBar/RightRideBar.jsx
@@ -8,7 +8,7 @@ import { IoGift } from "react-icons/io5";
 const RightRideBar = () => {
   return (
     <div className="w-full  ">
-      <div className="bg-[#FFFFFF] w-full px-2 shadow-md  rounded-lg">
+      <div className="bg-[#FFFFFF] w-full px-2 shadow-sm  rounded-lg">
         <div className="flex pb-6 justify-between text-gray-400 items-center py-4 mb-4 gap-7 p-2">
           <h2>Stories</h2>
           <CiMenuKebab className="text-2xl font-semibold"></CiMenuKebab>
@@ -81,12 +81,11 @@ const RightRideBar = () => {
         </div>
       </div>
 
-
       {/* birthday card */}
 
       <div className="my-10">
-        <div className="card w-full bg-[#39f0d7] shadow-xl image-full">
-          <figure>
+        <div className="card w-full bg-[#34ffe4]  shadow-xl image-full">
+          <figure className="relative">
             <Image
               height={400}
               className="opacity-40"
@@ -94,17 +93,35 @@ const RightRideBar = () => {
               src={birthdaycake}
               alt="Shoes"
             />
+            <div className="bg-[#45D7C4] bg-opacity-50 contrast-200 absolute h-[400px] w-[400px] top-0 ">
+             
+            </div>
           </figure>
+
           <div className="card-body">
-            <div className=" flex justify-between items-center">
+            <div className=" flex justify-between items-center mb-3">
               <IoGift className="text-2xl"></IoGift>
               <CiMenuKebab className="text-2xl  font-bold"></CiMenuKebab>
             </div>
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
+            <div className="card-title w-full relative mx-auto  text-center">
+              <Image
+                width={50}
+                height={50}
+                className="rounded-full mx-auto p-1 text-center  "
+                alt="Tailwind CSS Navbar component"
+                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+              />
+              <p className="bg-[#344258]  text-sm rounded-full absolute right-[36%] top-0">
+                34
+              </p>
             </div>
+            <p className="text-center">name turns 31 today!</p>
+            <p className="text-center text-sm ">
+              Send him your best wishes by leaving something on his wall.
+            </p>
+            <button className="btn btn-outline text-white hover:bg-white hover:text-black border-white">
+              Write message
+            </button>
           </div>
         </div>
       </div>
