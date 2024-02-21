3c07560aa07cdb7199073e71fa2945265cdbcab1
ashimroyrupok <ashimroyrupok@gmail.com>
2024-02-21 23:36:48 +0600

    created layout for homepage

--- src/app/(withLayout)/home/page.js
+++ src/app/(withLayout)/home/page.js
@@ -6,9 +6,13 @@ import React from "react";
 const HomePage = () => {
   return (
     <div>
-        <Navbar/>
-      <LeftSideBar />
-      <RightRideBar />
+      <div><Navbar /></div>
+      <div className="w-full mx-auto p-4 bg-[#F4F4F4] px-28 grid grid-cols-4 gap-3 mt-8 ">
+
+        <div className="col-span-1 ">   <LeftSideBar /> </div>
+        <div className=" col-span-2 bg-[#F4F4F4] "> News feed  </div>
+        <div className="colspan1 bg-[#F4F4F4]">  <RightRideBar /> </div>
+      </div>
     </div>
   );
 };
--- src/components/RightSideBar/RightRideBar.jsx
+++ src/components/RightSideBar/RightRideBar.jsx
@@ -1,13 +1,17 @@
-
-
-import React from 'react';
+"use client"
+import { CiMenuKebab } from "react-icons/ci";
 
 const RightRideBar = () => {
-    return (
-        <div>
-            <h2>Right Side Component </h2>
+  return (
+    <div>
+      <div className="bg-[#FFFFFF] w-full rounded-md">
+        <div className="flex justify-between gap-7 p-2">
+            <h2>Stories</h2>
+            <CiMenuKebab className="text-2xl font-semibold"></CiMenuKebab>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default RightRideBar;
