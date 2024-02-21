e42b214af4ad4bfcff0b7be7e340e6f782b40eba
Maruf Hossain <hossainmaruf001@gmail.com>
2024-02-21 17:42:23 +0100

    updated the navbar -maruf

--- src/components/LeftSideBar/LeftSideBar.jsx
+++ src/components/LeftSideBar/LeftSideBar.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const LeftSideBar = () => {
-    return (
-        <div>
-            <h2>Left Side Bar</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Left Side Bar</h2>
+    </div>
+  );
 };
 
 export default LeftSideBar;
--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,37 +1,43 @@
 import React from "react";
 import { CiHeart, CiBellOn } from "react-icons/ci";
+import { FaRegEnvelope } from "react-icons/fa";
+import { FaRegMessage } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
-    <div class="navbar bg-base-100">
-      <div class="flex-1">
-        <a class="btn btn-ghost text-xl">daisyUI</a>
-
-        <button class="btn btn-sm">
-          <CiHeart />
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl text-red-300 font-bold">
+          ConnectHUB
+        </a>
+        <button className="btn-sm">
+          <CiHeart></CiHeart>
         </button>
-
-        <button class="btn btn-sm">
-          <CiBellOn />
+        <button className="btn-sm">
+          <CiBellOn></CiBellOn>
         </button>
-        <button class="btn btn-sm">Button</button>
-        <button class="btn btn-sm">Button</button>
+        <button className="btn-sm">
+          <FaRegEnvelope></FaRegEnvelope>
+        </button>
+        <button className="btn-sm">
+          <FaRegMessage></FaRegMessage>
+        </button>
       </div>
-      <div class="flex-none gap-2">
-        <div class="form-control">
+      <div className="flex-none gap-2">
+        <div className="form-control">
           <input
             type="text"
             placeholder="Search"
-            class="input input-bordered w-24 md:w-auto"
+            className="input input-bordered w-24 md:w-auto"
           />
         </div>
-        <div class="dropdown dropdown-end">
+        <div className="dropdown dropdown-end">
           <div
-            tabindex="0"
+            tabIndex={0}
             role="button"
-            class="btn btn-ghost btn-circle avatar"
+            className="btn btn-ghost btn-circle avatar"
           >
-            <div class="w-10 rounded-full">
+            <div className="w-10 rounded-full">
               <img
                 alt="Tailwind CSS Navbar component"
                 src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
@@ -39,13 +45,13 @@ const Navbar = () => {
             </div>
           </div>
           <ul
-            tabindex="0"
-            class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+            tabIndex={0}
+            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
           >
             <li>
-              <a class="justify-between">
+              <a className="justify-between">
                 Profile
-                <span class="badge">New</span>
+                <span className="badge">New</span>
               </a>
             </li>
             <li>
