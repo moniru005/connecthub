0368a215573e9447433e80840bcca7a3cc10ab0c
Monir <moniru005@gmail.com>
2024-02-22 01:11:35 +0600

    create side menubar using headlessui by monir

--- src/components/LeftSideBar/LeftSideBar.jsx
+++ src/components/LeftSideBar/LeftSideBar.jsx
@@ -1,29 +1,13 @@
 import React from 'react';
 import { IconBase } from 'react-icons';
+import SideMenu from '../SideMenu/SideMenu';
 
 const LeftSideBar = () => {
-
-    const leftSideBarData = [
-        {
-            name: 'test1',
-            icon: IconBase,
-        }, 
-        {
-            name: 'test2',
-            icon: IconBase,
-        }, 
-    ]
-
 
 
     return (
         <div>
-            <h2>Left Side Bar</h2>
-            {
-                leftSideBarData.map(data=>{
-                    console.log(data.name)
-                })
-            }
+           <SideMenu/>            
         </div>
     );
 };
---
+++ src/components/SideMenu/SideMenu.jsx
@@ -0,0 +1,139 @@
+"use client";
+import { Disclosure } from "@headlessui/react";
+import { ChevronUpIcon } from "@heroicons/react/20/solid";
+import { CgFeed } from "react-icons/cg";
+import { IoIosImages } from "react-icons/io";
+import { BiSolidVideos } from "react-icons/bi";
+import { GoQuestion } from "react-icons/go";
+
+export default function SideMenu() {
+  return (
+    <div className="w-full px-4 ">
+      <div className="mx-auto w-full max-w-md rounded-2xl bg-white p-2">
+        
+        <Disclosure>
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex w-full justify-between rounded-lg  px-4 py-2 text-left text-md font-medium ">
+                <div className="flex flex-row gap-2 items-center">
+                <CgFeed />
+                <span>Feed</span>
+                </div>
+                <ChevronUpIcon
+                  className={`${
+                    open ? "rotate-180 transform" : ""
+                  } h-5 w-5 text-purple-500`}
+                />
+              </Disclosure.Button>
+
+              <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
+                <div className="flex flex-col gap-y-2 justify-start text-sm ">
+                  <button className="flex justify-start hover:bg-slate-300 hover:w-full w-full px-4 py-1 rounded">
+                    Trending
+                  </button>
+                  <button className="flex justify-start hover:bg-slate-300 hover:w-full w-full px-4 py-1 rounded">
+                    Popular
+                  </button>
+                  <button className="flex justify-start hover:bg-slate-300 hover:w-full w-full px-4 py-1 rounded">
+                    Following
+                  </button>
+                </div>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex w-full justify-between rounded-lg  px-4 py-2 text-left text-md font-medium ">
+                <div className="flex flex-row gap-2 items-center">
+                <IoIosImages/>
+                <span>Photos</span>
+                </div>
+                <ChevronUpIcon
+                  className={`${
+                    open ? "rotate-180 transform" : ""
+                  } h-5 w-5 text-purple-500`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
+              <div className="flex flex-col gap-y-2 justify-start text-sm ">
+                  <button className="flex justify-start hover:bg-slate-300 hover:w-full w-full px-4 py-1 rounded">
+                    Album
+                  </button>
+                  <button className="flex justify-start hover:bg-slate-300 hover:w-full w-full px-4 py-1 rounded">
+                    Recent Photos
+                  </button>
+                  
+                </div>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex w-full justify-between rounded-lg  px-4 py-2 text-left text-md font-medium ">
+                <div className="flex flex-row gap-2 items-center">
+                <BiSolidVideos />
+                <span>Videos</span>
+                </div>
+                <ChevronUpIcon
+                  className={`${
+                    open ? "rotate-180 transform" : ""
+                  } h-5 w-5 text-purple-500`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
+              <div className="flex flex-col gap-y-2 justify-start text-sm ">
+                  <button className="flex justify-start hover:bg-slate-300 hover:w-full w-full px-4 py-1 rounded">
+                    Suggested
+                  </button>
+                  <button className="flex justify-start hover:bg-slate-300 hover:w-full w-full px-4 py-1 rounded">
+                    My Channel
+                  </button>
+                  
+                </div>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex w-full justify-between rounded-lg  px-4 py-2 text-left text-md font-medium ">
+                <div className="flex flex-row gap-2 items-center">
+                <GoQuestion />
+                <span>Questions</span>
+                </div>
+                <ChevronUpIcon
+                  className={`${
+                    open ? "rotate-180 transform" : ""
+                  } h-5 w-5 text-purple-500`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
+              <div className="flex flex-col gap-y-2 justify-start text-sm ">
+                  <button className="flex justify-start hover:bg-slate-300 hover:w-full w-full px-4 py-1 rounded">
+                    Home
+                  </button>
+                  <button className="flex justify-start hover:bg-slate-300 hover:w-full w-full px-4 py-1 rounded">
+                    My Questions
+                  </button>
+                  <button className="flex justify-start hover:bg-slate-300 hover:w-full w-full px-4 py-1 rounded">
+                    Popular
+                  </button>
+                  
+                </div>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+
+      </div>
+    </div>
+  );
+}
