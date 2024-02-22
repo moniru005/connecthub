91701fef998952a81a34ca2a14ac66863fb38885
Monir <moniru005@gmail.com>
2024-02-22 22:37:23 +0600

    Create Login  and Register Form by Monir

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 
+import Login from "@/components/Login/Login";
 import React from "react";
-import HomePage from "./(withLayout)/home/page";
 
 const LandingPage = () => {
   return (
     <div>
-      <HomePage/>
+      <Login/>
     </div>
   );
 };
