f6d347546eb4ca88a82de7b4aa6404f7981bf378
Rahim_Badsa <rahimbadsa723@gmail.com>
2024-02-23 17:34:48 +0600

    useuser hook for get user info and use this on login page

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,17 +1,34 @@
 "use client"
 import React from "react";
 import { FaLock, FaUser } from "react-icons/fa";
+import useUser from "../Hooks/useUser";
 
 const LoginForm = ({show, setShow}) => {
+  const {login} = useUser()
+
+  const handleLogin = async(e)=>{
+    e.preventDefault()
+    const email = e.target.email.value
+    const password = e.target.password.value
+
+     await login(email, password)
+    .then((res)=>{
+      
+    })
+    .catch((err)=>console.log(err))
+
+  }
+
+
   return (
-    <form className="mt-6 w-full flex justify-center flex-col gap-y-4 items-center">
+    <form onSubmit={handleLogin} className="mt-6 w-full flex justify-center flex-col gap-y-4 items-center">
       <div className="relative">
         <FaUser className="absolute left-0 top-[10px] pl-2 text-xl text-gray-300" />
         <input
           className="w-96 border rounded py-2 pl-8 pr-2"
           type="text"
-          name=""
-          placeholder="jennadavis@gmail.com"
+          name="email"
+          placeholder="example@gmail.com"
         />
       </div>
 
@@ -20,13 +37,13 @@ const LoginForm = ({show, setShow}) => {
         <input
           className="w-96 border rounded py-2 pl-8 pr-2"
           type="text"
-          name=""
+          name="password"
           placeholder="Password"
         />
       </div>
 
       <div className="flex justify-center w-full">
-        <button className="btn w-96 rounded-full bg-[#5e9ae3] hover:bg-[#69A2E9]  text-white text-base">
+        <button type="submit" className="btn w-96 rounded-full bg-[#5e9ae3] hover:bg-[#69A2E9]  text-white text-base">
           Login
         </button>
       </div>
