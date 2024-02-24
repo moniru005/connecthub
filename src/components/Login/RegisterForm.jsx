33c9ed85724d464c814338804613d898fc61d0c4
ashimroyrupok <ashimroyrupok@gmail.com>
2024-02-25 00:21:25 +0600

    added user details in database and host user image

--- src/components/Login/RegisterForm.jsx
+++ src/components/Login/RegisterForm.jsx
@@ -1,48 +1,94 @@
 import React from "react";
-import { FaLock, FaUser } from "react-icons/fa";
+import { FaImage, FaLock, FaUser } from "react-icons/fa";
 import useUser from "../Hooks/useUser";
 import Swal from "sweetalert2";
 import { redirect, useRouter } from "next/navigation";
 import Loader from "@/app/loading";
 import useAxiosPublic from "../Hooks/useAxiosPublic";
 
+const api_key = "2ada4fdae6c29ea9b5ef757d310870c7";
+console.log(api_key);
+const hosting_api = `https://api.imgbb.com/1/upload?key=${api_key}`;
+
 const RegisterForm = ({ show, setShow }) => {
   const { createUser, updateUserProfile, user, loading } = useUser();
   const router = useRouter();
   const axiosPublic = useAxiosPublic();
 
-  const handleCreateUser = (e) => {
+  const handleCreateUser = async (e) => {
     e.preventDefault();
 
     const email = e.target.email.value;
     const password = e.target.password.value;
     const name = e.target.name.value;
-    const photo =
-      "https://i.ibb.co/hXQBM2X/jake-nackos-IF9-TK5-Uy-KI-unsplash.jpg";
+    const photo = e.target.image.files[0];
     const confirmPassword = e.target.confirmPassword.value;
     if (password == confirmPassword) {
-      createUser(email, password)
-        .then(async (res) => {
-          updateUserProfile(name, photo);
-          Swal.fire({
-            position: "center",
-            icon: "success",
-            title: "Successfully Registered",
-            showConfirmButton: false,
-            timer: 1500,
-          });
-          router.push("/home");
-        })
-        .catch((err) => {
-          Swal.fire({
-            position: "center",
-            icon: "warning",
-            title: "Error while Register",
-            showConfirmButton: false,
-            timer: 1500,
-          });
-        });
+      const imageFile = { image: photo };
+      console.log(imageFile);
+      const res = await axiosPublic.post(hosting_api, imageFile, {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      });
 
+      console.log(res.data, "successfully image hosted");
+
+      if (res.data.success) {
+        const image = res.data.data.display_url;
+
+        createUser(email, password)
+          .then(async (res) => {
+            updateUserProfile(name, image);
+
+            const userData = {
+              name,
+              email,
+              password,
+              image,
+            };
+
+            const result = await axiosPublic.post("/users", userData);
+            console.log(result.data);
+
+            if (result.data.insertedId) {
+              Swal.fire({
+                position: "center",
+                icon: "success",
+                title: "Successfully Registered",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+              router.push("/home");
+            }
+            else{
+                Swal.fire({
+                  position: "center",
+                  icon: "success",
+                  title: "Try again",
+                  showConfirmButton: false,
+                  timer: 1500,
+                });
+            }
+          })
+          .catch((err) => {
+            Swal.fire({
+              position: "center",
+              icon: "warning",
+              title: "Try with another email ",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          });
+      } else {
+        Swal.fire({
+          position: "center",
+          icon: "error",
+          title: "Try with another image",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
     } else {
       Swal.fire({
         position: "center",
@@ -80,6 +126,15 @@ const RegisterForm = ({ show, setShow }) => {
           type="text"
           name="name"
           placeholder="Full Name"
+        />
+      </div>
+      <div className="relative">
+        <FaImage className="absolute left-0 top-[10px] pl-2 text-xl text-gray-300" />
+        <input
+          className="w-96 border rounded py-2 pl-8 pr-2"
+          type="file"
+          name="image"
+          placeholder="Add your image"
         />
       </div>
 
